e29002f5bfdbbb8fe585e95c6a679f1ec9c69802
uoyniXnaH <b4baker@gmail.com>
2026-02-23 23:03:12 +0900

    initial commit

---
+++ weather_app/lib/weather_app.ex
@@ -0,0 +1,18 @@
+defmodule WeatherApp do
+  @moduledoc """
+  Documentation for `WeatherApp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WeatherApp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
