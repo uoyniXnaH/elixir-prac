7569cd5fece713de8e4856e34331356e0a39ea33
uoyniXnaH <b4baker@gmail.com>
2026-02-24 23:08:20 +0900

    basic practice about pattern match

--- weather_app/lib/weather_app.ex
+++ weather_app/lib/weather_app.ex
@@ -1,18 +1,24 @@
 defmodule WeatherApp do
-  @moduledoc """
-  Documentation for `WeatherApp`.
-  """
+  @doc "Return the current weather for the given city"
+  def get_weather(city) do
+    case city do
+      "Beijing" -> {:ok, "Sunny", 25}
+      "London" -> {:ok, "Cloudy", 15}
+      "New York" -> {:ok, "Rainy", 20}
+      _ -> {:error, "City not found"}
+    end
+  end
 
-  @doc """
-  Hello world.
+  def print_report(city) do
+    city
+    |> get_weather()
+    |> format_output()
+  end
 
-  ## Examples
-
-      iex> WeatherApp.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  defp format_output({:ok, condition, temp}) do
+    "The current weather is #{condition} with a temperature of #{temp}°C."
+  end
+  defp format_output({:error, message}) do
+    "Error: #{message}"
   end
 end
